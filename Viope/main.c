dbd19d2263dc0d1dd30e6935accee0607767689a
Kateridzhe <62830083+ekaterinadvolkova@users.noreply.github.com>
2021-04-12 02:28:27 +0300

    Calculating an average, loop structure
    Calculating an average, loop structure

--- Viope/main.c
+++ Viope/main.c
@@ -1,96 +1,31 @@
-/* Your task is to supplement the source code below with the missing functions. The program prints the following text in accordance with the user's selections:
-
-
-Choice 1: "The cat says meow!"
-Choice 2: "The dog says wuff-wuff!"
-Choice 3: "The cow says moo!"
-
-
-Upon execution, the program asks the user to select one of the animals. Once selected, the program calls a function that prints out the correct utterance. After executing the function, the program returns to ask the user for a new choice. The program is terminated by a negative integer.
-
-Code to be supplemented:
-
-
-#include <stdio.h>
-
-void cat(void);
-void dog(void);
-void cow(void);
-
-void main()
+#include<stdio.h>
+int main()
 {
-  int choice;
-  do{
+    int num_entrd, count = 0,sum=0;
+    float avg;
 
-    printf("1:cat\n2:dog\n3:cow\n");
-    printf("End with a negative number.\n");
-    printf("Select animal:");
-    scanf("%d", &choice);
-    if(choice < 0) {
-      printf("Terminating the program...");
-    }
-    else if(choice == 1) {
-      cat();
-    }
-    else if(choice == 2) {
-      dog();
-    }
-    else if(choice == 3) {
-      cow();
-    }
-    else {
-      printf("You entered an invalid number.\n\n");
-    }
-  }while(choice>0);
+    printf("The program calculates the average of scores you enter.\n");
+    printf("End with a negative integer.\n");
+    do
+    {
+        printf("Enter score (4-10):");
+        scanf("%d", &num_entrd);
+
+        if (num_entrd >= 4 && num_entrd <= 10)
+        {
+            count++;
+            sum = sum + num_entrd;
+            avg =(float)sum/ count;
+        }
+        else if (num_entrd <= 0)
+        {
+            printf("You entered %d scores.\n", count);
+            printf("Average score: %.2f\n", avg);
+        }
+    }while (num_entrd >= 4 && num_entrd <= 10);
+
+    return 0;
 }
 
-Hint: The purpose of the exercise is to provide you with a feeling for writing functions. Do not copy the source code, only write functions that print the output. The functions are declared in the prototypes before the main program.
-*/
 
-#include <stdio.h>
 
-void cat(void);
-void dog(void);
-void cow(void);
-
-void main()
-{
-    int choice;
-    do{
-
-        printf("1:cat\n2:dog\n3:cow\n");
-        printf("End with a negative number.\n");
-        printf("Select animal:");
-        scanf("%d", &choice);
-        if(choice < 0) {
-            printf("Terminating the program...");
-        }
-        else if(choice == 1) {
-            cat();
-        }
-        else if(choice == 2) {
-            dog();
-        }
-        else if(choice == 3) {
-            cow();
-        }
-        else {
-            printf("You entered an invalid number.\n\n");
-        }
-    }while(choice>0);
-}
-
-void cat(){
-
-    printf("The cat says meow!\n" );
-}
-
-void dog(){
-
-    printf("The dog says wuff-wuff!\n" );
-}
-
-void cow(){
-
-    printf("The cow says moo!\n");
-}
